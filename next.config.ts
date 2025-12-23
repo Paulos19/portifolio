810281b6de42cfe0cc001f22464fea26026e0854
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-12-22 22:17:15 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bzbrxkmhdxvh0b4p.public.blob.vercel-storage.com', // Domínio padrão do Vercel Blob
+      },
+    ],
+  },
 };
 
 export default nextConfig;
